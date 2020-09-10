bd170ab3d8a916bc0af8489149f7335378fbffbc
James Addison <jay@jp-hosting.net>
2020-09-10 14:12:07 +0100

    Add left & up orientations

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 export {default as sankey} from "./sankey.js";
 export {center as sankeyCenter, left as sankeyLeft, right as sankeyRight, justify as sankeyJustify} from "./align.js";
-export {orientRight, orientDown} from "./orientation.js";
+export {orientUp, orientDown, orientLeft, orientRight} from "./orientation.js";
 export {sankeyLinkHorizontal, sankeyLinkVertical} from "./sankeyLink.js";
--- src/orientation.js
+++ src/orientation.js
@@ -1,5 +1,13 @@
+export function orientUp(x0, y0, x1, y1) {
+  return {x0: y0, y0: x1, x1: y1, y1: x0};
+}
+
 export function orientDown(x0, y0, x1, y1) {
   return {x0: y0, y0: x0, x1: y1, y1: x1};
+}
+
+export function orientLeft(x0, y0, x1, y1) {
+  return {x0: x1, y0: y0, x1: x0, y1: y1};
 }
 
 export function orientRight(x0, y0, x1, y1) {
